e3b27b6c93092b87a195e29fef8c0a5afb66f94e
aaron marcus <aaronmarcus2007@gmail.com>
2023-02-02 22:33:48 +0100

    Implementation of simple health system

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,33 @@
-// MINILIB NEEDS TO BE IMPORTED IN HTML FIRST:
-// <script src="../scripts/minilib.js"></script>
-
-// Example functionality
-
+// Check if localStorage is supported
 if (!supportsLocalStorage()) alert('ERROR: Please use a browser that supports localStorage');
 
-let counter = localStorage.getItem('counter') || 0;
+// Global variables
 let level = localStorage.getItem('level') || 0;
 
+// onPageload
 window.onload = () => {
     setLocalStorage('level', 69);
     makeNewParent("div", "container");
     makePara("container", `Counter: ${counter}`);
     replacePara("level", `Level: ${getLocalStorage('level') || 0}`);
+    replacePara("health", "Health: 0");
+    setLocalStorage("health", 0);
+    setInnerHTML("gameWindow", "") // clear gameWindow
 }
+
+// Actions
+function damage(damage) {
+    setLocalStorage("health", parseInt(getLocalStorage('health')) - damage);
+    replacePara("health", `Health: ${getLocalStorage('health')}`);
+}
+
+function heal(heal) {
+    setLocalStorage("health", parseInt(getLocalStorage('health')) + heal);
+    replacePara("health", `Health: ${getLocalStorage('health')}`);
+}
+
+// Example functions (Counter)
+let counter = localStorage.getItem('counter') || 0;
 
 function makePara(parentID, text) {
     makeNewChild("p", parentID, "para");
