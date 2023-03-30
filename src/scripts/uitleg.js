6ad207c574087262a4665d8f4afb44de78b73cf0
aaron marcus <aaronmarcus2007@gmail.com>
2023-03-30 20:27:55 +0200

    Link uitleg to swal->uitleg

--- src/scripts/uitleg.js
+++ src/scripts/uitleg.js
@@ -1,11 +1,13 @@
-swal({
-    title: "Uitleg",
-    icon: "info",
-    button: "Ik begrijp het!",
-    text: `Welkom bij Levenskeuzes!
+function uitleg() {
+    swal({
+        title: "Uitleg",
+        icon: "info",
+        button: "Ik begrijp het!",
+        text: `Welkom bij Levenskeuzes!
     Dit is een spel waarin je keuzes moet maken die je leven beïnvloeden.
     Maar let op! één foute keuze en je bent weer terug bij af.
     Er staat twee grote knoppen met elke een keuze
     Je hebt het spel uitgespeeld zodra je alle beschikbare prestaties hebt behaald.
     Veel succes en leef een gezond leven.`,
-  });
+    });
+}
