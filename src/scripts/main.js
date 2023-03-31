2b415f80602964043d5cddc680232a1750853a67
AaronMarcusDev <74452139+AaronMarcusDev@users.noreply.github.com>
2023-03-31 13:42:38 +0200

    Mini update voor James

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,5 @@
 // onPageload
 window.onload = () => {
-    // clearLocalStorage();
-    // Check if localStorage is supported
     if (!supportsLocalStorage()) {
         swal("ERROR: Please use a browser that supports localStorage");
         swal("The game won't function properly without localStorage. Please try again in another browser.");
@@ -33,12 +31,9 @@ function death(message) {
             clearInterval(timerInterval)
         }
     }).then((result) => {
-        /* Read more about handling dismissals below */
-        if (result.dismiss === Swal.DismissReason.timer) {
-            console.log('I was closed by the timer')
-        }
+        Level1();
     })
-    Level1();
+    
 }
 
 function achievement(name, preInfo) {
@@ -187,6 +182,69 @@ function Level3() {
     replacePara("level", `Level: 3`);
     if (parseInt(getLocalStorage("highScore")) < 3) {
         setLocalStorage("highScore", 3);
+        replacePara("highScore", `Highscore: ${localStorage.getItem("highScore")}`);
+    }
+
+    let currentRound = 1;
+
+    leftOption = document.getElementById("leftOption");
+    rightOption = document.getElementById("rightOption");
+
+    initButtons("Neem een koude douche", "Neem een te warme douche");
+
+    leftOption.onclick = function () {
+        // l1
+        if (currentRound === 1) {
+            initButtons("Neem een vleesvervanger", "Eet een AVG'tje");
+            // l2
+        } else if (currentRound === 2) {
+            death("Vleesvervangers bevatten vaak veel zout, vet en suiker, wat slecht is voor je gezondheid op de lange termijn.");
+            // l3
+        } else if (currentRound === 3) {
+            if (!getLocalStorage("a3")) {
+                setLocalStorage("a3", true);
+                achievement("We waarderen de motivatie", "Lange periodes van leren zonder pauze kan leiden tot concentratieproblemen, vermoeidheid en hoofdpijn. ");
+            } else {
+                death("Lange periodes van leren zonder pauze kan leiden tot concentratieproblemen, vermoeidheid en hoofdpijn.");
+            }
+            // l4
+        } else if (currentRound === 4) {
+            if (!getLocalStorage("a4")) {
+                setLocalStorage("a4", true);
+                achievement("Cheat Day 1", "Als je dan toch een burger moet kiezen, dan is de Whopper Burger van de Burger King de 'gezondere' optie. ");
+            } else {
+                death("Als je dan toch een burger moet kiezen, dan is de Whopper Burger van de Burger King de 'gezondere' optie.");
+            }
+        }
+        currentRound++;
+    }
+
+    rightOption.onclick = function () {
+        // r1
+        if (currentRound === 1) {
+            death("Koud douchen is goed voor je gezondheid, omdat het je bloedsomloop stimuleert en je lichaam helpt om afvalstoffen af te voeren.\n\nTe warm douchen kan leiden tot een droge huid, hoofdpijn en duizeligheid.");
+            // r2
+        } else if (currentRound === 2) {
+            initButtons("3 uur leren met 1 uur pauze", "30 minuten leren met 5 minuten pauze");
+            // r3
+        } else if (currentRound === 3) {
+            initButtons("Eet een Big Mac van de McDonalds", "Eet een Whopper Burger van de Burger King");
+            // r4
+        } else if (currentRound === 4) {
+            if (!getLocalStorage("a5")) {
+                setLocalStorage("a5", true);
+                achievement("Cheat Day 2", "Als je dan toch een burger neemt, neem dan degene met minder kcal. ");
+            }
+            Level4();
+        }
+        currentRound++;
+    }
+}
+
+function Level4() {
+    replacePara("level", `Level: 4`);
+    if (parseInt(getLocalStorage("highScore")) < 4) {
+        setLocalStorage("highScore", 4);
         replacePara("highScore", `Highscore: ${localStorage.getItem("highScore")}`);
     }
 
